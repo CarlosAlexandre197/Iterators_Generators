198c7ae50aea3af71203bfb5197bd192ffd54397
Carlos Alexandre <113312099+CarlosAlexandre197@users.noreply.github.com>
2023-06-12 00:09:54 -0300

    Add files via upload
    Novo arquivo.

---
+++ Mod_03_S2.py
@@ -0,0 +1,17 @@
+def decorator_imprimir(func):
+    def inner(capital, taxa, tempo):
+        print("CAPITAL: " + str(capital) + " TAXA: " + str(taxa) + " TEMPO: " + str (tempo))
+        resultado = func(capital, taxa, tempo)
+        print(f'RESULTADO: {resultado}')
+        return func(capital, taxa, tempo)
+    return inner
+
+@decorator_imprimir
+def juros_simples(capital, taxa, tempo):
+    return capital * (taxa / 100) * tempo
+
+if __name__ == "__main__":
+    capital = int(input())
+    taxa = int(input())
+    tempo = int(input())
+    juros_simples(capital, taxa, tempo)
